b167342682c1a76c1255a0a358000dbf99c1190b
tushar <tbajaj@iitg.ac.in>
2021-10-19 15:22:31 +0530

    added home screen

--- naata_connection_app/components/Header.js
+++ naata_connection_app/components/Header.js
@@ -1,22 +1,26 @@
+import { Dimensions } from 'react-native';
 import React  from 'react';
 import {View} from 'react-native';
 import TouchableImage from './TouchableImage';
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 function Header({navigation,scene}){
     return (
-      <View style={{backgroundColor:'#1e1e1e', height:50, display: 'flex', alignItems:'center', flexDirection:'row', justifyContent:'space-between'}}>
+      <View style={{backgroundColor:'#FFFFFF', height:windowHeight*0.06, display: 'flex', alignItems:'center', flexDirection:'row', justifyContent:'space-between'}}>
         {(['Home','Schedule','Map'].includes(scene.route.name))?
           <TouchableImage onPress={() => navigation.openDrawer() }
           source={require('../assets/icons/drawer.png')} imageStyle={{height:25,width:25, margin:10}}/> :
           <TouchableImage onPress={()=> {navigation?navigation.goBack():scene.descriptor.navigation.jumpTo('Tabs')}} 
           source={require('../assets/icons/hdr_back.png')} imageStyle={{height:20,width:20, margin:10}}/>
         }
-        {(['Home','Schedule','Map'].includes(scene.route.name))?
+        {/* {(['Home','Schedule','Map'].includes(scene.route.name))?
           <TouchableImage onPress={() => {} }
           source={require('../assets/icons/hdr_notif.png')} imageStyle={{height:25,width:25, margin:10}}
           badge={scene.descriptor.options.headerNotification?<View style={{borderRadius:5, height:10, width:10, position:'absolute',top:0, right:0, backgroundColor:'#FF7388', borderWidth:1, borderColor:'#1e1e1e'}}/>:null}
           /> : null
-        }
+        } */}
       </View>
     );
 }
--- naata_connection_app/screens/home/Allevents.js
+++ naata_connection_app/screens/home/Allevents.js
@@ -1,190 +1,265 @@
 import { Assets } from '@react-navigation/stack';
-import { Dimensions } from 'react-native';
-import React, {useState, useEffect} from 'react';
+import { Button, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import FontAwesome, { SolidIcons, RegularIcons, BrandIcons } from 'react-native-fontawesome';
-import {ImageBackground, StyleSheet, Text, SafeAreaView, ScrollView, StatusBar,View, Image,TouchableHighlight, ActivityIndicator,TouchableWithoutFeedback, FlatList} from 'react-native';
+import { ImageBackground, StyleSheet, Text, SafeAreaView, ScrollView, StatusBar, View, Image, TouchableHighlight, ActivityIndicator, TouchableWithoutFeedback, FlatList } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { IconButton, Colors } from 'react-native-paper';
 import RectangleCard from '../../components/RectangleCard';
 import SquareCard from '../../components/SquareCard';
 import SearchBox from '../../components/SearchBox.js';
 import { useSelector, connect } from 'react-redux';
-import {setEvents} from '../../store/Action';
+import { setEvents } from '../../store/Action';
 import { event } from 'react-native-reanimated';
+import { orange, blue, red, green } from "../../constants/Color";
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { tsNonNullExpression } from '@babel/types';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-const RectangleMoreCard = (props) =>{
-  return (
-    <TouchableWithoutFeedback onPress={props.onPress}>
-      <View style={styles.rectmorecard}>
-        <Text style={styles.rectmorecardtext}>+10 More</Text>
-      </View>
-    </TouchableWithoutFeedback>
-    
-  );
+const RectangleMoreCard = (props) => {
+	return (
+		<TouchableWithoutFeedback onPress={props.onPress}>
+			<View style={styles.rectmorecard}>
+				<Text style={styles.rectmorecardtext}>+10 More</Text>
+			</View>
+		</TouchableWithoutFeedback>
+
+	);
 }
 
-const SquareMoreCard = () =>{
-  return (
-    <View style={styles.squamorecard}>
-      <Text style={styles.squamorecardtext}>+10 More</Text>
-    </View>
-  );
+const SquareMoreCard = () => {
+	return (
+		<View style={styles.squamorecard}>
+			<Text style={styles.squamorecardtext}>+10 More</Text>
+		</View>
+	);
 }
 
-const AllEvents = (props) => {  
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={{flexDirection:'column', flex:4}}>
-        <View style={styles.scrollView}>
-          <Text style={styles.heading}>Ongoing Events</Text>
-          <ScrollView  horizontal={true} style={{flexDirection:'row'}}>
-              <RectangleCard
-                key="1234"
-                tag="Reg. Open"
-                title="Demo"
-                subtitle="Subtitle Demo"
-                date="2020-01-01"
-                imageUrl="../../assets/Techniche.png"
-                // onPress={()=>{props.navigation.navigate('IndividualEvent',{e_id: event.e_id})}}
-              />
-            <RectangleMoreCard onPress={()=>{props.navigation.navigate('EventList')}}/>
-          </ScrollView>
+const AllEvents = (props) => {
+	return (
+		<SafeAreaView style={styles.container}>
+			<ScrollView style={{ flexDirection: 'column'}}>
+				<View style={styles.scrollView}>
+					<ImageBackground source={require('../../assets/naata_images/backgroundNotifications.png')} resizeMode="cover" style={styles.notificationsBackground}>
+						<Text style={styles.heading}>Notifications</Text>
+						<ScrollView horizontal={true} style={{ flexDirection: 'row' }}>
+							<RectangleCard
+								key="1234"
+								tag="Reg. Open"
+								title="Demo"
+								subtitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+								date="2020-01-01"
+								imageUrl="../../assets/Techniche.png"
+							// onPress={()=>{props.navigation.navigate('IndividualEvent',{e_id: event.e_id})}}
+							/>
+							<RectangleCard
+								key="12345"
+								tag="Reg. Open"
+								title="Demo"
+								subtitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+								date="2020-01-01"
+								imageUrl="../../assets/Techniche.png"
+							// onPress={()=>{props.navigation.navigate('IndividualEvent',{e_id: event.e_id})}}
+							/>		
+							<RectangleCard
+								key="12346"
+								tag="Reg. Open"
+								title="Demo"
+								subtitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+								date="2020-01-01"
+								imageUrl="../../assets/Techniche.png"
+							// onPress={()=>{props.navigation.navigate('IndividualEvent',{e_id: event.e_id})}}
+							/>					
+						</ScrollView>
+					</ImageBackground>
+				</View>
+
+				<View style={styles.attendanceView}>
+					<Text style={styles.attendanceText}>Attendance</Text>
+					<View style={styles.innerAttendanceView}>
+						<Text style={styles.attendanceDate}>
+							10-10-2021
+						</Text>
+						<View style={styles.day}>
+							<View style={styles.startDay}>
+								<TouchableHighlight>
+									<Text style={styles.startDayText}>
+										Start Day
+									</Text>
+								</TouchableHighlight>
+							</View>
+							<View style={styles.endDay}>
+								<TouchableHighlight>
+									<Text style={styles.endDayText}>
+										End Day
+									</Text>
+								</TouchableHighlight>
+							</View>
+						</View>
+					</View>
+				</View>
+        <View style={styles.entryView}>
+            <View style={styles.dieselView}>
+              <View style={styles.innerDieselView}>
+                <View style={{flex:1}}>
+                  <Text style={styles.dieselText}>
+                    Diesel Entry
+                  </Text>
+                </View>
+                <View style={styles.dieselRow}>
+                  <Text style={styles.newFormText}>
+                    Fill new form
+                  </Text>
+                  <TouchableHighlight>
+                        <Image source={require('../../assets/naata_images/nextButtonBlue.png')} style={styles.nextButton}></Image>
+                  </TouchableHighlight>
+                </View>
+              </View>
+            </View>
+            <View style={[styles.dieselView, {backgroundColor:"#00B4D8"}]}>
+              <View style={styles.innerDieselView}>
+                <View style={{flex:1}}>
+                  <Text style={styles.dieselText}>
+                    Other Expenses
+                  </Text>
+                </View>
+                <View style={styles.dieselRow}>
+                  <Text style={styles.newFormText}>
+                    Fill new form
+                  </Text>
+                  <TouchableHighlight>
+                        <Image source={require('../../assets/naata_images/nextButtonOrange.png')} style={styles.nextButton}></Image>
+                  </TouchableHighlight>
+                </View>
+              </View>
+            </View>
         </View>
-        
-        <View style={styles.scrollView}>
-        <Text style={styles.heading}>Upcoming Events</Text>
-          <ScrollView horizontal={true}>
-            {/* <SquareCard tag="Reg. Open" onPress={()=>{props.navigation.navigate('IndividualEvent',{e_id:'E21002'})}}></SquareCard> */}
-            <SquareCard tag="Starts in 5h"></SquareCard>
-            <SquareCard tag="Reg. Closed"></SquareCard>
-            <SquareMoreCard></SquareMoreCard>
-          </ScrollView>
-        </View>
-        <View style={styles.scrollView}>
-          <Text style={styles.heading}>You May Also Like</Text>
-          <ScrollView  horizontal={true}>
-              {/* <RectangleCard
-                key={event.id}
-                tag={event.details.tag}
-                title={event.name}
-                subtitle={event.details.module}
-                date={event.details.period}
-                // onPress={()=>{props.navigation.navigate('IndividualEvent',{e_id: event.e_id})}}
-              /> */}
-          <RectangleMoreCard/>
-          </ScrollView>
-        </View>
-      </ScrollView>
-    </SafeAreaView> 
-  );
+			</ScrollView>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  searchBoxContainer:{
-    paddingLeft: '8%'
-    // alignItems: 'center',
-
-  },
-  // upcomingevents:{
-  //   flex:2,
-  //   flexDirection:'row',
-  // },
-  cardcontainer:{
-    flex:3,
-    flexDirection:'column-reverse',
-    overflow:'hidden',
-  },
-  cardcontainerimage:{
-    overflow:'hidden',
-    height:80,
-    width:'100%',
-    position:'absolute',
-    bottom:0,
-    left:0,
-    right:0,
-    zIndex:-1,
-    borderRadius: 6,
-  },
-  cardimage: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    borderRadius:10,
-  },
-  cardtitle: {
-    color: '#FFFFFF',
-    fontSize: 20,
-    fontWeight:'700',
-    paddingLeft:10,
-    paddingTop:5,
-  },  
-  cardsubtitle:{
-    color: '#FFFFFF',
-    fontSize: 15,
-    paddingLeft:10,    
-  },
-  carddate:{
-    color: '#FFFFFF',
-    fontSize: 15,
-    paddingLeft:10,
-    paddingBottom:5,
-  },
-  container: {
-    flex: 1,
-    height:146,
+	notificationsBackground: {
+		height: windowHeight * 0.35,
+		width: windowWidth
+	},
+	container: {
+		alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+	},
+	heading: {
+		color: '#FFFFFF',
+		fontSize: 26,
+		lineHeight: 43,
+		fontWeight: "900",
+		fontFamily: 'Sofia Pro',
+		marginTop: windowHeight * 0.02,
+		marginLeft: windowWidth * 0.06
+	},
+	attendanceView: {
+		margin: windowWidth * 0.05,
+		padding: windowWidth * 0.05,
+		backgroundColor: "#CAF0F8",
+		borderRadius: 24,
+    width: windowWidth*0.9
+	},
+	innerAttendanceView: {
+		margin: windowWidth * 0.05,
+		padding: windowWidth * 0.02,
+		backgroundColor: "#FFFFFF",
+		flexDirection: "column",
+		alignItems: "center"
+	},
+	attendanceText: {
+		fontSize: 26,
+		fontWeight: "700",
+		fontFamily: "Sofia Pro"
+	},
+	attendanceDate: {
+		fontFamily: "Sofia Pro",
+		fontSize: 36,
+		fontWeight: "300",
+		marginBottom: windowHeight * 0.01
+	},
+	day: {
+		height: windowHeight * 0.125,
+		width: windowWidth * 0.65,
+		// backgroundColor: "#00B4D8",
+		flexDirection: "column",
+	},
+	startDay: {
+		flex: 1,
+		backgroundColor: "#00B4D8",
+		flexDirection: "column",
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginBottom: windowHeight*0.005,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16
+	},
+  endDay:{
+    flex:1,
+    backgroundColor: "#F3752B",
+    flexDirection: "column",
     alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomLeftRadius: 16,
+    borderBottomRightRadius: 16
   },
-  scrollView: {
-    marginHorizontal: 20,
+  startDayText:{
+    color: "#FFFFFF",
+    fontFamily: "Sofia Pro",
   },
-  text: {
-    fontSize: 42,
+  endDayText:{
+    color:"#FFFFFF",
+    fontFamily: "Sofia Pro"
   },
-  rectmorecard:{
-    backgroundColor:'#1E1E1E',
+  entryView:{
+		margin: windowWidth * 0.05,
+		// backgroundColor: "#CAF0F8",
+		borderRadius: 24,
+    width: windowWidth*0.9,
+    height: windowHeight*0.4
+  },
+  dieselView:{
     flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-    width: windowWidth*0.8,
-    height: windowHeight*0.25,
-    borderRadius: 8,
-    margin:10,
+    backgroundColor: "#F3752B",
+    marginBottom: windowHeight*0.028,
+    borderRadius:18
   },
-  rectmorecardtext:{
-    color:'rgba(255,255,255,0.6)',
+  innerDieselView:{
+    margin: windowWidth*0.02,
+    flexDirection: 'column',
+    height: windowHeight*0.2,
+    padding: windowWidth*0.01
+  }, 
+  dieselRow:{
+    flexDirection:'row',
+    flex: 3,
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
-  squamorecardtext:{
-    color:'rgba(255,255,255,0.6)',
-  },  
-  squamorecard:{
-    backgroundColor:'#1E1E1E',
+  dieselText:{
+    fontSize: 36,
+    fontWeight: '700',
+    fontFamily: "Sofia Pro",
+    color: '#FFFFFF',
+  },
+  newFormText:{
+    fontSize: 20,
+    fontWeight: '300',
+    fontFamily: "Sofia Pro",
+    color: '#FFFFFF',
+  },
+  otherView: {
     flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-    width: 170,
-    height: 250,
-    borderRadius: 8,
-    marginLeft:10,
-    marginTop:10,
-  },  
-  heading:{
-    color:'rgba(255,255,255,0.87)',
-    marginHorizontal:10,
-    fontSize:16,
-    lineHeight:15.71,
-    marginTop:20,
-  },
-  like:{
-    position:'absolute',
-    top:0,
-    right:2,
-  },
-  tag:{
-    color:"white",
-    fontFamily:'SF Pro Display',
+    backgroundColor: "#00B4D8",
+    marginBottom: windowHeight*0.028,
+    borderRadius:18
   }
 });
 
